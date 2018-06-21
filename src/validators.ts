ee88302b619da26869687c0b30b472ae163edc2b
David Kudera <kudera.d@gmail.com>
2018-06-21 07:49:37 +0200

    Simplify validators

--- src/validators.ts
+++ src/validators.ts
@@ -3,35 +3,31 @@ import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
 export function fileMaxSize(maxSize: number): ValidatorFn
 {
-	return (control: AbstractControl): ValidationErrors => {
-		if (!control.value) {
-			return null;
-		}
-
-		const files: FileList = control.value;
-		const errors: Array<any> = [];
-
-		for (let i = 0; i < files.length; i++) {
-			if (files[i].size > maxSize) {
-				errors.push(files[i]);
-			}
-		}
-
-		if (errors.length) {
-			return {
-				fileMaxSize: {
-					maxSize: maxSize,
-					invalidFiles: errors,
-				},
-			};
-		}
-
-		return null;
-	};
+	return fileValidation((file) => file.size > maxSize, (errors) => {
+		return {
+			fileMaxSize: {
+				maxSize: maxSize,
+				invalidFiles: errors,
+			},
+		};
+	});
 }
 
 
 export function fileType(allowedTypes: Array<string>): ValidatorFn
+{
+	return fileValidation((file) => allowedTypes.indexOf(file.type) < 0, (errors) => {
+		return {
+			fileType: {
+				allowedTypes: allowedTypes,
+				invalidFiles: errors,
+			},
+		};
+	});
+}
+
+
+function fileValidation(validator: (file: Blob) => boolean, createErrors: (errorFiles: Array<Blob>) => ValidationErrors): ValidatorFn
 {
 	return (control: AbstractControl): ValidationErrors => {
 		if (!control.value) {
@@ -42,18 +38,13 @@ export function fileType(allowedTypes: Array<string>): ValidatorFn
 		const errors: Array<any> = [];
 
 		for (let i = 0; i < files.length; i++) {
-			if (allowedTypes.indexOf(files[i].type) < 0) {
+			if (validator(files[i])) {
 				errors.push(files[i]);
 			}
 		}
 
 		if (errors.length) {
-			return {
-				fileType: {
-					allowedTypes: allowedTypes,
-					invalidFiles: errors,
-				},
-			};
+			return createErrors(errors);
 		}
 
 		return null;
